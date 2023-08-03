e5afa530c8145b1a681dd154fb31eeb32bdbb438
Ni9Logic <rehmanhassan117@gmail.com>
2023-08-03 12:26:17 +0000

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <main className='bg-[#f4f5ef]'>
       <NavBar />
       <div className='h-[89vh] flex items-center justify-center'>
-        <div className='flex max-sm:bject-fit h-[89vh] overflow-hidden'>
-          <Image src={main} alt='main' width={1300} height={1300} className='max-sm:transform max-sm:scale-110 max-sm:w-full bg-center bg-no-repeat bg-cover'/>
+        <div className='flex max-sm:bject-fit h-[89vh] overflow-hidden justify-center self-center'>
+          <Image src={main} alt='main' width={1300} height={1300} className='max-sm:transform max-sm:scale-110 max-sm:w-full bg-center bg-no-repeat bg-cover object-cover w-full'/>
         </div>
 
         <div className='h-screen'>
